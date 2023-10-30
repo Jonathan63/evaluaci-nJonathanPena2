03dc6e3e9676d90f68df6a1dd5bf421ea0045584
Jonathan Peña <67765142+Jonathan63@users.noreply.github.com>
2023-10-30 01:06:29 -0500

    Ingresar la posision iniical del alfil

---
+++ ejercicio2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la fila de la posición de partida (1-8): ");
+        int filaPartida = scanner.nextInt();
+
+        System.out.print("Ingrese la columna de la posición de partida (1-8): ");
+        int columnaPartida = scanner.nextInt();
+
+        
+    }
+}
